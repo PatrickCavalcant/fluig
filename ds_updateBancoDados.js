c9d74cb2fc23f37440461c4ff34d80075862003d
Patrick Cavalcante <54637677+PatrickCavalcant@users.noreply.github.com>
2025-03-11 21:33:53 -0300

    Update ds_updateBancoDados.js

--- ds_updateBancoDados.js
+++ ds_updateBancoDados.js
@@ -1,18 +1,29 @@
-function createDataset(fields, constraints, sortFields) {
+function defineStructure() { 
+	//Cria as colunas
+	addColumn("RETORNO");
+    setKey(["RETORNO"]);
+}
+
+function onSync(lastSyncDate) {
+	var dataset = DatasetBuilder.newDataset();
+
 	log.info("DEVDEBUG => #01 Start Dataset");
     var dataSource = "/jdbc/AppDS";
     var ic = new javax.naming.InitialContext();
     var ds = ic.lookup(dataSource);
-    
 
     var myQuery = null;
 
-    
-    myQuery = "UPDATE TAR_PROCES " +
-		  "SET CD_MATRICULA = '7403652' " +
-		  "WHERE  NUM_PROCES = '41761' AND NUM_SEQ_MOVTO = '12'";
+//    myQuery = "UPDATE ML001103  " +
+//		  "INNER JOIN PROCES_WORKFLOW ON ML001103.DOCUMENTID = PROCES_WORKFLOW.NR_DOCUMENTO_CARD " +
+//		  "SET ML001103.STATUS = 'Fechado' " +
+//		  "WHERE " +
+//		  "ML001103.STATUS != 'Fechado' " +
+//		  "AND PROCES_WORKFLOW.COD_DEF_PROCES = 'wf_sistema_chamados' " +
+//		  "AND PROCES_WORKFLOW.STATUS = 2";
    
-    
+    myQuery = "UPDATE ML001103 SET TELEFONE = '(65) 3324-9292' WHERE DOCUMENTID = '14078'"
+    	
     try {
         log.info("##################### " + myQuery)
         var conn = ds.getConnection();
@@ -20,19 +31,20 @@ function createDataset(fields, constraints, sortFields) {
 //      var rs = stmt.executeQuery(myQuery);
 //      var rs = stmt.execute(query);
         var rs = stmt.executeUpdate(myQuery);
+		dataset.addRow(['UPDATE EXECUTADO COM SUCESSO!']);
         
     } catch (e) {
-        log.info("DEVDEBUG => #11 ERROR DATASET: " + e.message);
+        log.error("DEVDEBUG => #11 ERROR DATASET: " + e.message);
     } finally {
-        if (rs != null) {
-            rs.close();
-        }
+		log.info("==================>FINALLY<===============");
         if (stmt != null) {
             stmt.close();
         }
         if (conn != null) {
             conn.close();
+
         }
     }
-    return 0;
+	return dataset;
+
 }
